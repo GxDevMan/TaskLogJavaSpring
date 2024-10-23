b46f6b3d8babe8f453ecf62148bbf5b144757468
Feivel Doctora <fvdoctora@gmail.com>
2024-10-23 16:23:47 +0800

    java spring setup

---
+++ src/main/java/com/todoTask/taskLog/TaskLogApplication.java
@@ -0,0 +1,13 @@
+package com.todoTask.taskLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoTask/taskLog/TaskLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoTask.taskLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
